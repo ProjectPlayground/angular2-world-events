52a405327f1486b7dbbeaa2779cb1f67b6df08c6
Richard Gathogo <muraguri2005@gmail.com>
2017-02-22 09:17:18 +0300

    add angular material

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,9 +3,11 @@ import {AngularFire, FirebaseListObservable} from "angularfire2";
 @Component({
   moduleId:module.id,
   selector:'home',
-  templateUrl:'home.component.html'
+  templateUrl:'home.component.html',
+  styleUrls:['home.component.css']
 })
 export class HomeComponent{
+  isDarkTheme = true;
   events:FirebaseListObservable<any[]>;
   constructor(private angularFire:AngularFire){
     this.events = angularFire.database.list("/events");
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from "@angular/core";
 import {HomeRouting} from "./home.routing";
 import {HomeComponent} from "./home.component";
 import {CommonModule} from "@angular/common";
+import {MaterialModule} from "@angular/material";
 @NgModule({
   imports:[
     HomeRouting,
-    CommonModule
+    CommonModule,
+    MaterialModule
   ],
   declarations:[
     HomeComponent
